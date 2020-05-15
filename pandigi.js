92d42d1ef1b616dd7b2746e611cf7557244ed59e
muratbigin <muratbigin00@gmail.com>
2020-05-15 05:11:44 +0200

    pandigital number checking algorithm solution

---
+++ pandigi.js
@@ -0,0 +1,30 @@
+// A pandigital number contains all digits(0 - 9) at least once.Write a
+// function that takes an integer, returning true
+// if the integer is pandigital, and false otherwise.
+
+// Examples
+// isPandigital(98140723568910)➞ true
+
+// isPandigital(90864523148909)➞ false
+// // 7 is missing.
+
+// isPandigital(112233445566778899)➞ false
+
+
+/////////////////////
+
+const t1 = 132458760
+let t2 = 98140723568910
+const t3 = 112233445566778899n //2^53 ten buyuk sayilar icin sona 'n' koy...
+
+const controleArr = Array.from({
+    length: 10
+}, (v, k) => k)
+
+const compare = (param) => {
+    const res = controleArr.filter(i =>
+        !param.toString().includes(`${i}`)
+    )
+    return res.length > 0 ? `${res} false` : true
+}
+console.log("ali", compare(t1))
